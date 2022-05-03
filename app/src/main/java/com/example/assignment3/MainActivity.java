ac0f40e4ed756abc58deb6f03e2e417b10a82926
PippinBo <zboo0001@student.monash.edu>
2022-05-03 20:01:40 +1000

    test1

--- app/src/main/java/com/example/assignment3/MainActivity.java
+++ app/src/main/java/com/example/assignment3/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //4564564
+        //gggiggiggithub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
