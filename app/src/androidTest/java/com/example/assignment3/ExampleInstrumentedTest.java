ae7511a095f97e67c921c51adbbf56a1122ea762
LichenWang1997 <lichenwang1997@outlook.com>
2022-05-09 16:18:22 +1000

    Test

--- app/src/androidTest/java/com/example/assignment3/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/assignment3/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.assignment3.fitbud", appContext.getPackageName());
+        assertEquals("com.example.assignment3", appContext.getPackageName());
     }
 }
---
+++ app/src/main/java/com/example/assignment3/SignupActivity.java
@@ -0,0 +1,5 @@
+package com.example.assignment3;
+
+public class SignupActivity {
+    //just for test
+}
