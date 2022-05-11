329d9e847b79b4c18ca2b15118b0bb36d00ecf4d
benson <blin0008@student.monash.edu>
2022-05-12 02:48:29 +1000

    Add record UI implemented

--- app/src/main/java/com/example/assignment3/ui/record/RecordFragment.java
+++ app/src/main/java/com/example/assignment3/ui/record/RecordFragment.java
@@ -1,9 +1,12 @@
 package com.example.assignment3.ui.record;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -19,6 +22,8 @@ import com.example.assignment3.adapter.MovementAdapter;
 import com.example.assignment3.databinding.FragmentRecordBinding;
 import com.example.assignment3.model.MovementTest;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 public class RecordFragment extends Fragment {
@@ -26,12 +31,14 @@ public class RecordFragment extends Fragment {
     private ArrayList<MovementTest> recordList;
     private FragmentRecordBinding binding;
     private RecyclerView recyclerView;
+    private Button addRecordButtonDialog;
+    private MovementAdapter adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) { RecordViewModel recordViewModel =
                 new ViewModelProvider(this).get(RecordViewModel.class);
 
 
-        //binding = FragmentRecordBinding.inflate(inflater, container, false);
+        binding = FragmentRecordBinding.inflate(inflater, container, false);
         //View root = binding.getRoot();
 
         View root = LayoutInflater.from(getContext()).inflate(R.layout.fragment_record,container,false);
@@ -40,13 +47,44 @@ public class RecordFragment extends Fragment {
         setMovementInfo();
         setAdapter();
 
+        addRecordButtonDialog = root.findViewById(R.id.addRecordButton);
+        addRecordButtonDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(root.getContext());
+                dialog.setContentView(R.layout.layout_add_record);
+                TextView dateTxt = dialog.findViewById(R.id.dateTextView);
+                EditText editDistance = dialog.findViewById(R.id.addRecordDistance);
+                Button confirmButton = dialog.findViewById(R.id.confirmAddRecord);
+
+                confirmButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+
+                    public void onClick(View view) {
+                        String distance = "";
+                        String date  = "";
+                        distance = editDistance.getText().toString();
+                        date = dateTxt.getText().toString();
+
+                        recordList.add(0,new MovementTest("21/10/2022",distance));
+                        adapter.notifyItemInserted(0);
+                        recyclerView.scrollToPosition(0);
+
+                        dialog.dismiss();
+
+                    }
+                });
+                dialog.show();
+            }
+        });
+
         //final TextView textView = binding.textRecord;
         //recordViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
     private void setAdapter() {
-        MovementAdapter adapter = new MovementAdapter(recordList);
+        adapter = new MovementAdapter(recordList);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
