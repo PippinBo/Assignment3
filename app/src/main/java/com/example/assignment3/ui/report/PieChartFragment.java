b777818ea7cd52b47452b272a9ed8473d8f83786
benson <blin0008@student.monash.edu>
2022-05-12 17:50:16 +1000

    Added sample pie-chart

--- app/src/main/java/com/example/assignment3/ui/report/PieChartFragment.java
+++ app/src/main/java/com/example/assignment3/ui/report/PieChartFragment.java
@@ -1,6 +1,7 @@
 package com.example.assignment3.ui.report;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,9 +17,19 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.assignment3.R;
 import com.example.assignment3.databinding.FragmentPiechartBinding;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class PieChartFragment extends Fragment {
 
+    private PieChart pieChart;
     private FragmentPiechartBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -31,7 +42,6 @@ public class PieChartFragment extends Fragment {
 
 
         Button ID = (Button) root.findViewById(R.id.back_button);
-
         ID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,9 +54,60 @@ public class PieChartFragment extends Fragment {
             }
         });
 
+        pieChart = root.findViewById(R.id.pieReportChart);
+        setupPieChart();
+        loadPieChartData();
+
+
+
         final TextView textView = binding.textPieChart;
         pieChartViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    private void setupPieChart(){
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setUsePercentValues(true);
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setCenterText("Travel Distance");
+        pieChart.setCenterTextSize(24);
+        pieChart.getDescription().setEnabled(false);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setEnabled(true);
+
+    }
+
+    private void loadPieChartData(){
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(0.4f, "12/02/2021"));
+        entries.add(new PieEntry(0.6f, "13/02/2021"));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int color: ColorTemplate.MATERIAL_COLORS) {
+            colors.add(color);
+        }
+
+        for (int color: ColorTemplate.VORDIPLOM_COLORS){
+            colors.add(color);
+        }
+
+        PieDataSet pieDataSet = new PieDataSet(entries, "FitBud");
+        pieDataSet.setColors(colors);
+
+        PieData data = new PieData(pieDataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+
+        pieChart.setData(data);
+        pieChart.invalidate();
     }
 
     @Override
