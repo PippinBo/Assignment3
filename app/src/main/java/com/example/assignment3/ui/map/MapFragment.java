6bcbad2464172e59e8f818f975044b8e4ace477f
HongyuYang296 <hongyuyang084@gmail.com>
2022-05-14 01:10:43 +1000

    add find nearly gym

--- app/src/main/java/com/example/assignment3/ui/map/MapFragment.java
+++ app/src/main/java/com/example/assignment3/ui/map/MapFragment.java
@@ -1,34 +1,66 @@
 package com.example.assignment3.ui.map;
 
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Address;
 import android.location.Geocoder;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.assignment3.MainActivity;
 import com.example.assignment3.R;
 import com.example.assignment3.entity.User;
 import com.example.assignment3.ui.record.RecordViewModel;
+import com.example.assignment3.ui.report.FacebookActivity;
 import com.example.assignment3.viewmodel.UserViewModel;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
+
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PlacesApi;
+import com.google.maps.model.PlaceType;
+import com.google.maps.model.PlacesSearchResponse;
+import com.google.maps.model.PlacesSearchResult;
+import com.google.maps.model.RankBy;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOError;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 //version 1.0.1: map fragment --Hongyu
@@ -50,7 +82,9 @@ public class MapFragment extends Fragment {
         Bundle bundle = getActivity().getIntent().getExtras();
         User user = bundle.getParcelable("loginUser");
         String address = user.getAddress();
-//        System.out.println(address);
+
+
+
 
 
 //
@@ -62,49 +96,121 @@ public class MapFragment extends Fragment {
 //               String address = user.getAddress();
 //            }
 //        });
-
         // Async map
         assert supportMapFragment != null;
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
-                googleMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(-37.915047, 145.129272) , 12.0f) );
+                PlacesSearchResult[] placesSearchResults = NearlyPlace.run().results;
+                Context context = requireActivity().getApplicationContext();
+
+
+
+                int height = 130;
+                int width = 130;
+                @SuppressLint("UseCompatLoadingForDrawables")
+                BitmapDrawable bitmapdraw = (BitmapDrawable)getResources().getDrawable(R.drawable.ic_gym);
+                Bitmap b = bitmapdraw.getBitmap();
+                Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+
+                for (PlacesSearchResult placesSearchResult : placesSearchResults) {
+                    double lat = placesSearchResult.geometry.location.lat;
+                    double lng = placesSearchResult.geometry.location.lng;
+
+                    googleMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(lat, lng))
+                            .title(placesSearchResult.name)
+                            .icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+                    );
+                }
+
+
+                LatLng latLng=getLocationFromAddress(context,
+                        address );
+                googleMap.moveCamera( CameraUpdateFactory.newLatLngZoom(latLng , 12.0f) );
                 googleMap.setMinZoomPreference(6.0f);
                 googleMap.setMaxZoomPreference(14.0f);
 
+                Button zoomOutButton = (Button) view.findViewById(R.id.zoom_out);
+                zoomOutButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        googleMap.animateCamera(CameraUpdateFactory.zoomOut());
+                    }
+                });
 
-                Context context = requireActivity().getApplicationContext();
-                LatLng latLng=getLocationFromAddress(context,
-                       address );
+
+
+                Button zoomInButton = (Button) view.findViewById(R.id.zoom_in);
+                zoomInButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        googleMap.animateCamera(CameraUpdateFactory.zoomIn());
+                    }
+                });
+
 
                 googleMap.addMarker(new MarkerOptions()
                         .position(latLng)
-                        .title("Marker in user1")
+                        .title("You are here")
                         .draggable(true));
             }
         });
+
+         NearlyPlace.run();
+//        PlacesSearchResult[] results22 = request.results;
+//        String test1 = results22.toString();
+//        System.out.println(request);
+//        System.out.println(results22);
+//        System.out.println(test1);
 
         // Return view
         return view;
     }
 
 
+//
 
+
+    // get the lat and long from address
     public LatLng getLocationFromAddress(Context context, String strAddress) {
         Geocoder geocoder = new Geocoder(context);
+        //if the address is valid use this default address
+        String defaultAddress = "350 Bourke Street, Melbourne";
         List<Address> addressList;
         LatLng latLng = null;
-        try {
-            addressList = geocoder.getFromLocationName(strAddress, 1);
-            if (addressList!=null) {
-                Address location = addressList.get(0);
-                latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            try {
+                addressList = geocoder.getFromLocationName(strAddress, 1);
+                if (addressList!=null) {
+                    // This try catch can handle IndexOutOfBoundsException
+                    // Because if address is valid List<Address>.get(0)
+                    // will through IndexOutOfBoundsException
+                    try {
+                        Address location = addressList.get(0);
+                        latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    } catch (IndexOutOfBoundsException ex1) {
+                        addressList = geocoder.getFromLocationName(defaultAddress, 1);
+                        Address location = addressList.get(0);
+                        latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    }
+                }
+            } catch (Exception ex2) {
+                ex2.printStackTrace();
             }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            return latLng;
         }
-        return latLng;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //
